7a3f55b9ec10c9b6b6aa518d739122a9f98599ff
rohan <rohan.khatri121@gmail.com>
2021-12-10 14:33:33 +0530

    Initial Branch Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+  
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
         <p>
-        Rohan
+          Rohan
         </p>
-        
       </header>
     </div>
   );
